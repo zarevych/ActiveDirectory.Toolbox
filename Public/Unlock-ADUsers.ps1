1c70dbe82da72d8f7ef96e03ec6918e6475b11f7
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-04-10 03:16:12 +0300

    Create Unlock-ADUsers.ps1

---
+++ Public/Unlock-ADUsers.ps1
@@ -0,0 +1,29 @@
+﻿#Requires -Modules ActiveDirectory
+
+function Unlock-ADUsers {
+
+<#
+.SYNOPSIS
+ Unlock all Active Directory user accounts 
+
+.DESCRIPTION 
+ Unlock all Active Directory user accounts 
+ 
+ Requirement:
+ - Active Directory PowerShell Module  
+
+   File Name  : Unlock-ADUsers.ps1
+   Author     : Andriy Zarevych
+
+   Find me on :
+   * My Blog  :	https://angry-admin.blogspot.com/
+   * LinkedIn :	https://linkedin.com/in/zarevych/
+   * Github   :	https://github.com/zarevych
+#>
+
+    [CmdletBinding()]
+    Param (
+    )
+
+    Search-ADAccount -lockedout | Unlock-ADAccount
+}
