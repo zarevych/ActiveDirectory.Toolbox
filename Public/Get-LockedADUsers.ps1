7553d7258d83b4a559a34808532e7818b1f5ecb7
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-05-13 17:03:10 +0300

    Update Get-LockedADUsers.ps1

--- Public/Get-LockedADUsers.ps1
+++ Public/Get-LockedADUsers.ps1
@@ -14,15 +14,21 @@ function Get-LockedADUsers () {
 
 .NOTES
    File Name  : Get-LockedADUsers.ps1
+   Version    : 1.2005
    Author     : Andriy Zarevych
 
    Find me on :
    * My Blog  :	https://angry-admin.blogspot.com/
    * LinkedIn :	https://linkedin.com/in/zarevych/
    * Github   :	https://github.com/zarevych
+
+   Change Log:
+   V1.2004    : Initial version
+   V1.2005    : Add Get-ADUser Properties badpwdcount, lockoutTime, LockedOut, EmailAddress, Enabled
 #>
 
-    Search-ADAccount -Lockedout |
-    Select-Object Name, SamAccountName, UserPrincipalName, DistinguishedName | Sort-Object Name | Format-Table -AutoSize
-    #Select-Object Name, SamAccountName, UserPrincipalName, DistinguishedName #| Sort-Object Name | Format-Table -AutoSize
+    Search-ADAccount -Lockedout | 
+     Get-ADUser -Properties badPwdCount, lockoutTime, LockedOut, EmailAddress, Enabled |
+     Select-Object Name, SamAccountName, UserPrincipalName, DistinguishedName, badPwdCount, LockedOut, EmailAddress, @{ Name = "LockoutTime"; Expression = { ([datetime]::FromFileTime($_.lockoutTime).ToLocalTime()) } }, Enabled |
+     Sort-Object Name
 } 
