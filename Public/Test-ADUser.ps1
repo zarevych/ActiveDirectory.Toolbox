81e1a68652a295779cf1dd633b52469cf637009f
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-11-25 14:37:46 +0200

    Create Test-ADUser.ps1

---
+++ Public/Test-ADUser.ps1
@@ -0,0 +1,32 @@
+﻿#Test-ADUser/Check-ADUser v.0.2002
+
+#Requires -Modules ActiveDirectory
+#Requires -Version 2.0
+
+Function Test-ADUser {
+
+    [CmdletBinding()]
+    [OutputType([bool])]
+
+    param(
+        [Parameter(ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Mandatory = $true,
+        Position=0)]
+        [ValidateNotNullOrEmpty()]     
+        [Alias('UserName','Name')]
+        [String]$User = $env:USERNAME
+    )  
+
+    Write-Verbose "Check AD User: $User"
+    try {
+        if (Get-ADUser $User) {
+            Write-Verbose "Find object: $user"
+            Return $true
+        }
+    }
+    catch {
+        Write-Verbose $_.Exception.Message
+        Return $false
+    }
+}
